2d32f8cb965fe7f1067f0ec3f0ae2bb9b436072a
Andrew Parks and Lois Choi <lois@>
2017-07-05 14:32:45 -0700

    add toggle,hide and functions and add css style

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("button#cat").click(function(){
     alert("Bark!");
+    $("ul.catSays").append("<li>Meow!!!</li>");
   });
   $("button#dog").click(function() {
     alert("Meow!");
+    $("ul.dogSays").append("<li>Bark!!!</li>");
+  });
+  $(".clickable").click(function() {
+    $(".hide_kitty").toggle();
+      $("img").toggle();
   });
 });
+
 /*  $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
