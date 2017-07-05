3a3801fcc06f7a849c46400447317fb81ce48585
Andrew Parks and Lois Choi <andrew@>
2017-07-05 11:56:31 -0700

    add js and css, jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
   $("button#hello").click(function(){
-    $("ul").prepend("<li>Hello</li>");
+    $("ul#user").prepend("<li>Hello</li>");
+    $("ul#webpage").prepend("<li>Why hello there!");
+    $("li").css("background-color", "green");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
   $("button#goodbye").click(function() {
-    $("ul").prepend("<li>Goodbye!</li>");
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("li").css("background-color", "green");
+
   });
   $("button#stop").click(function() {
-    $("ul").prepend("<li>Stop copying me!</li>");
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("li").css("background-color", "green");
   });
 });
